79de474163ddc0833f040bffb9879bd68028607d
vivalchemy <vivianludrick.social+github@gmail.com>
2023-10-01 17:00:47 +0530

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { ShoppingBag, ShoppingCart } from 'lucide-svelte';
+  import { Button } from "$lib/components/ui/button";
+</script>
+
+<div class="flex justify-between items-start w-screen " >
+  <div class="relative top-6 left-4">
+    <Button variant="ghost" class="underline underline-offset-4">Contact Us</Button>
+  </div> 
+  <div class="font-serif text-9xl text-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">BELLA ALCHEVI</div>
+  
+<div class="flex flex-nowrap relative top-6 right-4">
+  <Button variant="ghost">
+    <ShoppingBag class="h-4 w-4 mx-1" />
+    Bag
+  </Button>
+  
+  <Button variant="ghost" on:click>
+    <ShoppingCart class="h-4 w-4 mx-1"/>
+    Cart
+  </Button>
+</div>
+  
+</div>
---
+++ tailwind.config.js
@@ -0,0 +1,66 @@
+import { fontFamily } from "tailwindcss/defaultTheme";
+
+/** @type {import('tailwindcss').Config} */
+const config = {
+	darkMode: ["class"],
+	content: ["./src/**/*.{html,js,svelte,ts}"],
+	theme: {
+		container: {
+			center: true,
+			padding: "2rem",
+			screens: {
+        sm: { min: "0px", max: "640px" },
+        md: { min: "641px", max: "1080px" },
+        lg: { min: "1080px", max: "1440px" },
+      },
+		},
+		extend: {
+			colors: {
+				border: "hsl(var(--border))",
+				input: "hsl(var(--input))",
+				ring: "hsl(var(--ring))",
+				background: "hsl(var(--background))",
+				foreground: "hsl(var(--foreground))",
+				primary: {
+					DEFAULT: "hsl(var(--primary))",
+					foreground: "hsl(var(--primary-foreground))"
+				},
+				secondary: {
+					DEFAULT: "hsl(var(--secondary))",
+					foreground: "hsl(var(--secondary-foreground))"
+				},
+				destructive: {
+					DEFAULT: "hsl(var(--destructive))",
+					foreground: "hsl(var(--destructive-foreground))"
+				},
+				muted: {
+					DEFAULT: "hsl(var(--muted))",
+					foreground: "hsl(var(--muted-foreground))"
+				},
+				accent: {
+					DEFAULT: "hsl(var(--accent))",
+					foreground: "hsl(var(--accent-foreground))"
+				},
+				popover: {
+					DEFAULT: "hsl(var(--popover))",
+					foreground: "hsl(var(--popover-foreground))"
+				},
+				card: {
+					DEFAULT: "hsl(var(--card))",
+					foreground: "hsl(var(--card-foreground))"
+				}
+			},
+			borderRadius: {
+				lg: "var(--radius)",
+				md: "calc(var(--radius) - 2px)",
+				sm: "calc(var(--radius) - 4px)"
+			},
+      fontFamily: {
+        sans: ["Poppins", ...fontFamily.sans],
+        serif: ["Bodoni\\ Moda",...fontFamily.serif],
+      },
+		}
+	},
+};
+
+export default config;
